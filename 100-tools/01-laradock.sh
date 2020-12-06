51021b305b04f737c79f62f49f321cd0da2f1949
JoaoLucasXavier <lucasxavier000@gmail.com>
2020-12-05 23:42:37 -0300

    20201205 2342

--- 100-tools/01-laradock.sh
+++ 100-tools/01-laradock.sh
@@ -3,5 +3,6 @@
 - LARADOCK -
 -> https://github.com/laradock/laradock
 -> https://www.youtube.com/watch?v=6XfZLqoywz4
+-> https://www.youtube.com/watch?v=lKhlG4xHKGM
 
 '
---
+++ 99-dev environment/01-php-env/01-php-env.sh
@@ -0,0 +1,15 @@
+:'
+- Ambiente de Desenvolvimento com Docker + Php + SSL local -
+-> https://www.youtube.com/watch?v=mc4FIQMEy5o
+-> https://github.com/studioatual/docker-nginx-php-pgsql-ssl
+-> https://github.com/studioatual/docker-apache-php-mysql-phpmyadmin-ssl
+
+
+
+- DOCKER LAMPP -
+-> https://github.com/jcavat/docker-lamp
+-> https://github.com/mzazon/php-apache-mysql-containerized
+-> https://github.com/theandersonn/docker-apache-php7-mysql
+
+
+'
