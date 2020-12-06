61e2a0939fa1342dafbf8ad1712ea43b95f24a7e
JoaoLucasXavier <lucasxavier000@gmail.com>
2020-12-06 16:47:54 -0300

    20201206 1646

--- 06-connect-container-cli/04-interactive.sh
+++
@@ -1,6 +0,0 @@
-:'
-1)A flag docker run [--interactive] ou [-i]:
-    -> Mantenha STDIN aberto, mesmo que não esteja conectado
-    -> Permite manter a entrada padrão |shell| aberta
-    -> Quando usamos [--interactive], não precisamos usar o [--attch], ele já vai fazer isso por padrão além de manter a entrada ativa/aberta.
-'
--- 06-connect-container-cli/05-tty.sh
+++
@@ -1,6 +0,0 @@
-:'
-1)A flag docker run [--tty] ou [-t]:
-    -> Alocar um pseudo-TTY
-    -> Podemos pegar um container e entrar em seu terminal para fazer alguma tarefa.
-2
-'
