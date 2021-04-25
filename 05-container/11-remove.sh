c28fab8505076684836af43b07229e35fddd2620
JoaoLucasXavier <lucasxavier000@gmail.com>
2021-04-25 10:44:57 -0300

    20210425

--- 05-container/11-remove.sh
+++ 05-container/11-remove.sh
@@ -1,13 +1,9 @@
-:'
+-> REMOVE UM CONTAINER -
+- CLI: docker container rm container
+- CLI: docker container rm container1
 
+-> REMOVE CONTAINERS INATIVOS -
+- CLI: docker container prune
 
-- REMOVE UM CONTAINER -
--> CLI: docker container rm container
--> CLI: docker container rm container1
-
-
-- REMOVE CONTAINERS INATIVOS -
--> CLI: docker container prune
-
-
-'
+-> REMOVER CONTAINER DE FORMA AUTONATIZADA
+- CLI: docker container rm $(docker container ls -aq) -f
