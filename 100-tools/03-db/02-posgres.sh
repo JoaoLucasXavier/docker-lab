3a544cd8d9f545dd20b9fb7f9decf070e5af1c20
JoaoLucasXavier <lucasxavier000@gmail.com>
2021-04-25 10:38:56 -0300

    20210425

--- 100-tools/03-db/02-posgres.sh
+++
@@ -1,3 +0,0 @@
-:'
-1)docker run --name postgres -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=password -e POSTGRES_DB=heroes -p 5432:5432 -d postgres
-'
--- 100-tools/03-db/03-mongodb.sh
+++
@@ -1,9 +0,0 @@
-:'
-1)docker run --name mongodb -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=password -d mongo:4
-
-2)Create new use and new database in mongodb:
-    -> docker exec -it mongodb mongo --host localhost -u admin -p password --authenticationDatabase admin --eval "db.getSiblingDB('herois').createUser({user: 'lucasxavier', pwd: 'password', roles: [{role: 'readWrite', db: 'herois'}]})"
-
-3) Mongodb client
-    -> docker run --name mongoclient -p 3000:3000 --link mongodb:mongodb -d mongoclient/mongoclient
-'
